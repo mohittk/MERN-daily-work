f4527bc1ee8e9b6686ce6e206f99f4dee150cfdb
Mohit Khairnar <79041510+mohittk@users.noreply.github.com>
2021-10-27 23:48:14 +0530

    Update app.js

--- day1/app.js
+++ day1/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-const DB = 'mongodb+srv://Mohit:mk142434@cluster0.dc5ep.mongodb.net/prac1?retryWrites=true&w=majority';
 mongoose.connect(DB).then(()=>{
     console.log('connection successful');
 }). catch(err => console.log(err));
