b24a1f48a1364c4b21a7c4b93dc9736db919984b
mohittk <mvk1407@gmail.com>
2021-11-21 23:54:50 +0530

    added client side folder

---
+++ clientside/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar'
+
+function App() {
+  return(
+    <>
+ 
+    <Navbar />
+    
+    </>
+  )
+  
+}
+
+export default App;
